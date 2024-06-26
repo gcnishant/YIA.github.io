669a2ffee22e57e0420497333193babf35c80628
gcnishant <156033774+gcnishant@users.noreply.github.com>
2024-06-26 12:44:37 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 
-const hiddenElements = document.querySelectorAll('.hidden,.contact-us,.our-mission,.become-a-partner,.who-we-are');
+const hiddenElements = document.querySelectorAll('.hidden,.contact-us,.our-mission,.become-a-partner,.who-we-are,.frequent-questions,.credits');
 hiddenElements.forEach((el) => observer.observe(el))
 
