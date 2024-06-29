d50f127f48d47f55df6d17da774ffbe30e8f5d2e
gcnishant <156033774+gcnishant@users.noreply.github.com>
2024-06-29 13:52:50 -0400

    Update Jun 29 2024

--- app.js
+++ app.js
@@ -12,3 +12,14 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden,.contact-us,.our-mission,.become-a-partner,.who-we-are,.frequent-questions,.credits');
 hiddenElements.forEach((el) => observer.observe(el))
 
+
+window.addEventListener('scroll', function() {
+    var header = document.getElementById('fixed-header');
+    if (window.scrollY > 50) { // Adjust the scroll value as needed
+        header.classList.add('smaller');
+    } else {
+        header.classList.remove('smaller');
+    }
+});
+
+
